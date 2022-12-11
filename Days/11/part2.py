2b44a86c4a4920cd44494ff3d0358d854df2a268
Aris <64918822+Arisamiga@users.noreply.github.com>
2022-12-11 23:45:17 +0000

    Day 11

---
+++ Days/11/part2.py
@@ -0,0 +1,56 @@
+
+# Part 2 of Day 11
+
+
+inputFile = open("./Days/11/input.txt", "r")
+
+inputValues = inputFile.read().strip().split("\n")
+
+rounds = 10000
+
+Monkeys = []
+
+for i in range(len(inputValues)):
+    value = inputValues[i].replace(":", "")
+    value = value.replace(",", "")
+    split = value.split(" ")
+    tempList = []
+    if split[0] == "Monkey":
+        numbers = inputValues[i + 1].replace("Starting items:", "").replace(" ", "").split(",")
+        tempList.append([int(x) for x in numbers])
+        tempList.append(inputValues[i + 2].split()[3:])
+        tempList.append(inputValues[i + 3].split()[3:])
+        tempList.append(inputValues[i + 4].split()[5:])
+        tempList.append(inputValues[i + 5].split()[5:])
+        Monkeys.append(tempList)
+
+interacted = [0] * len(Monkeys)
+
+
+modular = 1
+for i in Monkeys:
+    modular *= int(i[2][0])
+
+
+def calculate(number, operation):
+    lcal = number if operation[0] == 'old' else int(operation[0])
+    rcal = number if operation[2] == 'old' else int(operation[2])
+    if operation[1] == '+':
+        return (lcal + rcal) % modular
+    elif operation[1] == '*':
+        return (lcal * rcal) % modular
+
+for round in range(rounds):
+    for index in range(len(Monkeys)):
+        currentMonkey = Monkeys[index]
+        for item in currentMonkey[0]:
+            interacted[index] += 1
+            value = calculate(item, currentMonkey[1])
+            if value % int(currentMonkey[2][0]) == 0:
+                Monkeys[int(currentMonkey[3][0])][0].append(value)
+            else:
+                Monkeys[int(currentMonkey[4][0])][0].append(value)
+        currentMonkey[0].clear()
+
+interacted = sorted(interacted, reverse=True)
+print(interacted[0] * interacted[1])
