91aef7507b4f67b53256e24107f1b2f0d527dcc7
Aris <64918822+Arisamiga@users.noreply.github.com>
2022-12-14 23:59:20 +0000

    Day 14

---
+++ Days/14/part2.py
@@ -0,0 +1,64 @@
+
+# Part 2 of Day 14
+
+
+inputFile = open("./Days/14/input.txt", "r")
+
+inputValues = inputFile.read().split("\n")
+
+sandSource = 500,0
+
+alreadyFilled = set()
+
+for line in inputValues:
+    coordinates = []
+    for string in line.split(" -> "):
+        x, y = map(int, string.split(","))
+        coordinates.append([x, y])
+
+    for i in range(1, len(coordinates)):
+        currentX, currentY = coordinates[i]
+        previousX, previousY = coordinates[i - 1]
+
+        if currentY != previousY:
+            for y in range(min(currentY, previousY), max(currentY, previousY) + 1):
+                alreadyFilled.add((currentX, y))
+
+        if currentX != previousX:
+            for x in range(min(currentX, previousX), max(currentX, previousX) + 1):
+                alreadyFilled.add((x, currentY))
+                
+maximumY = max([coordinates[1] for coordinates in alreadyFilled])
+
+
+def sandFalling():
+    x,y = sandSource
+
+    while y <= maximumY:
+        if (x,y + 1) not in alreadyFilled:
+            y += 1
+            continue
+        if (x - 1, y + 1) not in alreadyFilled:
+            x -= 1
+            y += 1
+            continue
+        if (x + 1, y + 1) not in alreadyFilled:
+            x += 1
+            y += 1
+            continue
+        break
+
+    if (x,y) in alreadyFilled:
+        return (x,y)
+    return x,y
+
+
+answer = 0
+while True:
+    x,y = sandFalling()
+    alreadyFilled.add((x,y))
+    answer += 1
+    if (x,y) == sandSource:
+        break
+
+print(answer)
